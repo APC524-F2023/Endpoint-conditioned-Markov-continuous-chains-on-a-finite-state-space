e5c9833ff7341d5274113fc1ca851cbea6fadccc
Jiayu Li <jiayuli0204@gmail.com>
2023-11-29 15:40:40 -0500

    change uniformsampling

--- src/ECMC/uniformsampling.py
+++ src/ECMC/uniformsampling.py
@@ -13,12 +13,12 @@ def uniform_sampling(Q, T, a, b):
     path = []
 
     u = random.uniform(0, 1)
-    sum = 0
+    summ = 0
     n = 0
-    while sum < u:
+    while summ < u:
         n = n + 1
         Rn = np.linalg.matrix_power(R, n)
-        sum = sum + np.exp(mu*T)*(mu*T)**n/math.factorial(n)*Rn[a,b]/P[a,b]
+        summ = summ + np.exp(mu*T)*(mu*T)**n/math.factorial(n)*Rn[a,b]/P[a,b]
 
     if n == 0:
         for i in np.linspace(0, T, num = 100):
@@ -43,8 +43,11 @@ def uniform_sampling(Q, T, a, b):
         for i in range(n):
             Rp1 = np.linalg.matrix_power(R, n - i - 1)
             Rp1 = Rp1[ : , b]
+            Rp2 = np.linalg.matrix_power(R, n-i)
+            Rp2 = Rp2[ x , b]
+            nominator = np.multiply(R[x, :], Rp1)
             x = random.choices(population = range(d),
-                               weights = np.multiply(R[x, :], Rp1),
+                               weights = nominator/Rp2,
                                k = 1)[0]
             path.append((x, tau[i]))
         path.append((b, T))
--- tests/test_uniformsampling.py
+++ tests/test_uniformsampling.py
@@ -1,10 +1,11 @@
 import numpy as np
-
-from uniformsampling import uniform_sampling
+import ECMC
 
 
 def test_uniform():
-    Q = np.array([[-1, 0.5, 0.5],
-              [0.3, -0.8, 0.5],
-              [0.4, 0.2, -0.6]])
-    uniform_sampling(Q, 10, 0, 2)
+    Q = np.array([[1, -0.5, -0.5],
+              [-0.3, 0.8, -0.5],
+              [-0.4, -0.2, 0.6]])
+    result = uniformsampling(Q, 10, 0, 2)
+    assert isinstance(result, list)
+    assert len(result) > 0
