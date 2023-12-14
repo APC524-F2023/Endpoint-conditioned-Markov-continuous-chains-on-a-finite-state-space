fb9abf5f508d7158adcc48040d1c2b1b30e15e6d
Chenxiao Tian <62594979+Chenxiao-Tian@users.noreply.github.com>
2023-12-14 16:10:06 -0600

    Update test_directsampling.py

--- tests/test_directsampling.py
+++ tests/test_directsampling.py
@@ -1,16 +1,50 @@
+import unittest
 import numpy as np
-from ECMC.directsampling import directsampling
+from ECMC.directsample-generalcase import sample_path, path_prob, P_matrix, Muv  # import necessary functions
 
+class TestMarkovChainMethods(unittest.TestCase):
 
-def test_directsampling():
-    Q = np.array([[-1, 0.5, 0.5], [0.3, -0.8, 0.5], [0.4, 0.2, -0.6]])
-    T = 13
-    result = directsampling(Q, T)
+    def setUp(self):
+        # Initialize a transition matrix and other constants used in multiple tests
+        self.T = np.array([[0, 1/2, 1/2, 0, 0, 0, 0],
+                           [1/3, 0, 1/3, 1/3, 0, 0, 0],
+                           [1/2, 1/2, 0, 0, 0, 0, 0],
+                           [0, 1/4, 0, 0, 1/4, 1/4, 1/4],
+                           [0, 0, 0, 1/2, 0, 1/2, 0],
+                           [0, 0, 0, 1/3, 1/3, 0, 1/3],
+                           [0, 0, 0, 1/2, 0, 1/2, 0]])
+        self.a = 1
+        self.b = 7
+        self.N = 4
 
-    # Test for output type
-    assert isinstance(result, list), "Output should be a list"
+    def test_sample_path_length(self):
+        path = sample_path(self.a, self.b, self.N, self.T)
+        self.assertEqual(len(path), self.N + 1)  # Path length should be N + 1
 
-    # Test for non-empty output
-    assert len(result) > 0, "Output list should not be empty"
+    def test_sample_path_validity(self):
+        # Test if the start and end states of the path are correct
+        path = sample_path(self.a, self.b, self.N, self.T)
+        self.assertEqual(path[0], self.a)
+        self.assertEqual(path[-1], self.b)
 
-    # More tests can be added here, like checking the length, structure, etc.
+    def test_path_prob(self):
+        path = np.array([1, 2, 4, 6, 7], dtype=int)
+        prob = path_prob(path, self.a, self.b, self.N, self.T)
+        self.assertGreaterEqual(prob, 0)
+        self.assertLessEqual(prob, 1)  # Probability should be between 0 and 1
+
+    def test_P_matrix(self):
+        u, v = 2, 3
+        P = P_matrix(self.a, self.b, u, v, 1, self.N, self.T)
+        self.assertGreaterEqual(P, 0)
+        self.assertLessEqual(P, 1)  # Matrix element should be a valid probability
+
+    def test_Muv(self):
+        u, v = 2, 3
+        M = Muv(self.a, self.b, u, v, self.N, self.T)
+        self.assertIsInstance(M, float)  # Ensure Muv returns a float value
+
+# Add more test methods as needed
+
+if __name__ == '__main__':
+    unittest.main()
