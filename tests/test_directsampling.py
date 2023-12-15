804c1a4c23876571ec86da1e801410ac9b560a1d
Chenxiao Tian <ct3471@princeton.edu>
2023-12-14 18:15:58 -0600

    revise

--- tests/test_directsampling.py
+++ tests/test_directsampling.py
@@ -1,14 +1,18 @@
+import random
+
+import numpy as np
+from ECMC.directsampling import directsampling
 def test_directsampling():
     assert "hello" == "hello"
-    # Q = np.array([[-1, 0.5, 0.5], [0.3, -0.8, 0.5], [0.4, 0.2, -0.6]])
-    # T = 13
-    # result = directsampling(Q, T)
+    Q = np.array([[-1, 0.5, 0.5], [0.3, -0.8, 0.5], [0.4, 0.2, -0.6]])
+    T = 13
+    result = directsampling(Q, T)
 
     # # Test for output type
-    # assert isinstance(result, list), "Output should be a list"
+    assert isinstance(result, list), "Output should be a list"
 
-    # # Test for non-empty output
-    # assert len(result) > 0, "Output list should not be empty"
+    #Test for non-empty output
+    assert len(result) > 0, "Output list should not be empty"
 
 
 
